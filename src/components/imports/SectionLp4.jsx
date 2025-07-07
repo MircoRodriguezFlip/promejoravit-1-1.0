a8189392a15d4c65586f57abe7f8d43baadc6d45
mircomania <mirco.rodriguez@hotmail.com>
2025-07-07 18:51:51 -0400

    4 commit promejoravit 1 - section 3 y 4 full

--- src/components/imports/SectionLp4.jsx
+++ src/components/imports/SectionLp4.jsx
@@ -1,7 +1,34 @@
+import styles from '../../styles/modules/sectionLp4.module.css';
+
+import img1Section from '../../assets/images/img-section-lp-4.png';
+import { iconosSectionLp4 } from '../utils/iconosSectionLp4';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 export const SectionLp4 = () => {
     return (
-        <section>
-            <h2>section 4</h2>
+        <section className={styles.sectionContainer}>
+            <div className={styles.primerContenido}>
+                {iconosSectionLp4.map(({ id, titulo, items }) => (
+                    <div key={id} className={styles.preguntas}>
+                        <h2 className="bold-text">{titulo}</h2>
+
+                        {items.map(({ id, icon, texto }) => (
+                            <div key={id} className={styles.iconContainer}>
+                                <FontAwesomeIcon icon={icon} className={styles.icon} />
+                                <p className="light-text">{texto}</p>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+
+            <img
+                src={img1Section}
+                alt="Ejecutiva de Mejoravit mostrando el dinero que puedes recibir con Mejoravit"
+                loading="lazy"
+                decoding="async"
+            />
         </section>
     );
 };
---
+++ src/components/utils/iconosSectionLp4.js
@@ -0,0 +1,36 @@
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+
+export const iconosSectionLp4 = [
+    {
+        id: 1,
+        titulo: '¿Para qué sirve?',
+        items: [
+            {
+                id: '1a',
+                icon: faCircleCheck,
+                texto: 'Remodelar o ampliar tu vivienda',
+            },
+            {
+                id: '1b',
+                icon: faCircleCheck,
+                texto: 'Obtener dinero en efectivo con tu subcuenta de vivienda',
+            },
+        ],
+    },
+    {
+        id: 2,
+        titulo: '¿Cuánto puedo recibir?',
+        items: [
+            {
+                id: '2a',
+                icon: faCircleCheck,
+                texto: 'Hasta $163,000',
+            },
+            {
+                id: '2b',
+                icon: faCircleCheck,
+                texto: 'Depende del saldo y capacidad de pago del solicitante',
+            },
+        ],
+    },
+];
