72647e2b9aef466d8a81ff112b1afaddc64b6eb9
mircomania <mirco.rodriguez@hotmail.com>
2025-07-08 14:39:36 -0400

    6 commit promejoravit 1 - se agrega flecha a la home y se ajusta el css de imagenes y textos para no seleccion

--- src/components/imports/SectionLp1.jsx
+++ src/components/imports/SectionLp1.jsx
@@ -1,5 +1,6 @@
 import styles from '../../styles/modules/sectionLp1.module.css';
 import img1Section from '../../assets/images/img-section-lp-1.webp';
+import imgFlecha from '../../assets/images/flecha-home.webp';
 
 import { BotonNav } from '../utils/BotonNav';
 
@@ -15,10 +16,12 @@ export const SectionLp1 = () => {
             </header>
 
             <div className={styles.sectionContenido}>
-                <img src={img1Section} alt="Ejecutiva de Mejoravit presentando las ganancias del crédito" />
+                <img src={img1Section} alt="Ejecutiva de Mejoravit presentando las ganancias del crédito" className={styles.img1Section} />
 
                 <div className={styles.contenido1}>
                     <BotonNav dataCta="home-btn-1" />
+
+                    <img src={imgFlecha} alt="Flecha apuntando al botón de precalificación del crédito Mejoravit" className={styles.imgFlecha} />
 
                     <div>
                         <h2 className="light-text">Precalifica y obten hasta $163,000 en efectivo</h2>
