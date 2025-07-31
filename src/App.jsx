ccb93ed2bfde623642816ea67fea948b4871b3e3
mircomania <mirco.rodriguez@hotmail.com>
2025-07-31 16:48:07 -0400

    15 commit promejoravit 1 - se agrega carga de 1.5 seg y pagina de error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { NavBar } from './components/common/NavBar';
@@ -9,27 +9,45 @@ import { ScrollToTop } from './hooks/ScrollTop';
 const LandingPage = lazy(() => import('./components/pages/LandingPage'));
 const PoliticasPage = lazy(() => import('./components/pages/PoliticasPage'));
 const FaqPage = lazy(() => import('./components/pages/FaqPage'));
+const ErrorPage = lazy(() => import('./components/pages/ErrorPage'));
 
 function App() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 1500);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <BrowserRouter>
             <ScrollToTop />
 
             <NavBar />
 
-            <Suspense
-                fallback={
-                    <main className="cargando">
-                        <Cargando />
-                    </main>
-                }
-            >
-                <Routes>
-                    <Route path="/" element={<LandingPage />}></Route>
-                    <Route path="/politica-privacidad" element={<PoliticasPage />}></Route>
-                    <Route path="/faq" element={<FaqPage />}></Route>
-                </Routes>
-            </Suspense>
+            {loading ? (
+                <main className="cargando">
+                    <Cargando />
+                </main>
+            ) : (
+                <Suspense
+                    fallback={
+                        <main className="cargando">
+                            <Cargando />
+                        </main>
+                    }
+                >
+                    <Routes>
+                        <Route path="/" element={<LandingPage />}></Route>
+                        <Route path="/politica-privacidad" element={<PoliticasPage />}></Route>
+                        <Route path="/faq" element={<FaqPage />}></Route>
+                        <Route path="*" element={<ErrorPage />}></Route>
+                    </Routes>
+                </Suspense>
+            )}
 
             <Footer />
         </BrowserRouter>
--- src/components/utils/Cargando.jsx
+++ src/components/utils/Cargando.jsx
@@ -1,9 +1,14 @@
 import logoCargando from '../../assets/images/logo-navbar.webp';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
 export const Cargando = () => {
     return (
         <div className="cargando-container">
             <img src={logoCargando} alt="Logotipo de Mejoravit" />
+
+            <FontAwesomeIcon icon={faSpinner} spin size="2x" className="spinner-icon" />
         </div>
     );
 };
